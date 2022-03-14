40bdb38651873388ba887811321cdcf5ec35fb47
Min-Jae-Bae <qoalswo123@icloud.com>
2022-03-14 23:44:11 +0900

    2022-3-14

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
     </header>
     <div class="row">
     <nav class="col-md-3">
-        <ol>
+        <ol class="nav nav-pills nav-stacked">
         <?php
         while($row = mysqli_fetch_assoc($result)){
             echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
@@ -31,22 +31,24 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
         </ol>
     </nav>
     <div class="col-md-9">
-    <div id="control">
-        <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
-        <input type="button" value="black" onclick="document.getElementById('target').className='black'"/>
-    <a href="http://localhost/write.php">쓰기</a>
-    </div>
-    <article>
-    <?php
+        <article>
+            <?php
     if(empty($_GET['id']) == false) {
-    $sql = 'SELECT * FROM topic WHERE id = '.$_GET['id'];
-    $result = mysqli_query($conn, $sql);
-    $row = mysqli_fetch_assoc($result);
-    echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
-    echo strip_tags($row['discription'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
+        $sql = 'SELECT * FROM topic WHERE id = '.$_GET['id'];
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+        echo strip_tags($row['discription'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
     }
     ?>
     </article>
+    <div id="control">
+        <div class="btn-group" role="group" aria-label="...">
+            <input type="button" value="white" onclick="document.getElementById('target').className='white'" class="btn btn-default"/>
+            <input type="button" value="black" onclick="document.getElementById('target').className='black'" class="btn btn-default"/>
+        </div>
+        <a href="http://localhost/write.php" class="btn btn-success">쓰기</a>
+    </div>
     </div>
     </div>
     </div>
--- write.php
+++ write.php
@@ -8,38 +8,72 @@ $result = mysqli_query($conn, "SELECT * FROM topic");
 <html>
 <head>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="http://localhost/bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="http://localhost/style.css">
 </head>
 <body id="target">
-    <header>
-        <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩">
+    <div class="container-fluid">
+    <header class="jumbotron text-center">
+        <img src="https://s3.ap-northeast-2.amazonaws.com/opentutorials-user-file/course/94.png" alt="생활코딩" class="img-circle" id="logo">
         <h1><a href="http://localhost/index.php">JavaScript</a></h1>
     </header>
-    <nav>
-        <ol>
+    <div class="row">
+    <nav class="col-md-3">
+        <ol class="nav nav-pills nav-stacked">
         <?php
         while($row = mysqli_fetch_assoc($result)){
-            echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.$row['title'].'</a></li>'."\n";
+            echo '<li><a href="http://localhost/index.php?id='.$row['id'].'">'.htmlspecialchars($row['title']).'</a></li>'."\n";
         }
         ?>
         </ol>
     </nav>
-    <div id="control">
-        <input type="button" value="white" onclick="document.getElementById('target').className='white'"/>
-        <input type="button" value="black" onclick="document.getElementById('target').className='black'"/>
-    <a href="http://localhost/write.php">쓰기</a>
-    </div>
+    <div class="col-md-9">
+        <article>
+            <?php
+    if(empty($_GET['id']) == false) {
+        $sql = 'SELECT * FROM topic WHERE id = '.$_GET['id'];
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
+        echo '<h2>'.htmlspecialchars($row['title']).'</h2>';
+        echo strip_tags($row['discription'], '<a><h1><h2><h3><h4><h5><ul><ol><li>');
+    }
+    ?>
+    </article>
     <form action="process.php" method="post">    
-        <p>
-            제목 : <input type="text" name="title"> 
-        </p>
-        <p>
-            작성자 : <input type="text" name="author">
-        </p>
-        <p>
-            본문 : <textarea name="discription"></textarea>
-        </p>
-        <input type="submit" name="name">
+
+        <div class="form-group">
+        <label for="form-title">제목</label>
+        <input type="text" class="form-control" name="title" id="form-title" placeholder="제목을 적어주세요">
+        </div>
+
+        <div class="form-group">
+        <label for="form-author">작성자</label>
+        <input type="text" class="form-control" name="title" id="form-author" placeholder="작성자를 적어주세요">
+        </div>    
+
+        <div class="form-group">
+        <label for="form-author">작성자</label>
+        <textarea class="form-control" rows="10" name="title" id="form-author" placeholder="본문을 적어주세요"></textarea>
+        </div>    
+
+        <input type="submit" name="name" class="btn btn-default btn-lg">
     </form>
+    <div id="control">
+        <div class="btn-group" role="group" aria-label="...">
+            <input type="button" value="white" onclick="document.getElementById('target').className='white'" class="btn btn-default btn-lg"/>
+            <input type="button" value="black" onclick="document.getElementById('target').className='black'" class="btn btn-default btn-lg"/>
+        </div>
+        <a href="http://localhost/write.php" class="btn btn-success btn-lg">쓰기</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    <!-- jQuery necessary for Bootstrap's JavaScript plugins -->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins below, or include individual files as needed -->
+    <script src="http://localhost/bootstrap-3.3.4-dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
 </body>
 </html>
